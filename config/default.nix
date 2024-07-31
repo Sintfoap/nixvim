77927802a9d81a6db727d068078231211890080b
Ryan Moffitt <rmoffitt@visiostack.com>
2024-07-30 22:43:05 -0400

    Batman

---
+++ config/default.nix
@@ -0,0 +1,192 @@
+{pkgs, ...}: let
+  ruff = "${pkgs.ruff}/bin/ruff";
+  stylua = "${pkgs.stylua}/bin/stylua";
+  alejandra = "${pkgs.alejandra}/bin/alejandra";
+  isort = "${pkgs.isort}/bin/isort";
+in {
+  opts = {
+    number = true;
+    shiftwidth = 2;
+  };
+
+  globals = {
+    mapleader = " ";
+    maplocalleader = " ";
+  };
+
+  colorschemes.onedark = {
+    enable = true;
+    settings = {
+      style = "deep";
+    };
+  };
+
+  plugins = {
+    conform-nvim = {
+      enable = true;
+
+      formattersByFt = {
+        lua = ["stylua"];
+        nix = ["alejandra"];
+        python = ["isort" "ruff_format"];
+        "*" = ["trim_whitespace"];
+      };
+      formatters = {
+        ruff_format = {
+          command = ruff;
+          prepend_args = ["format"];
+        };
+        stylua.command = stylua;
+        alejandra.command = alejandra;
+        isort.command = isort;
+      };
+    };
+
+    lsp = {
+      enable = true;
+      inlayHints = true;
+      onAttach = ''vim.keymap.set("n", "<leader>f", function() require("conform").format({ async = true, lsp_fallback = true }) end) '';
+      keymaps.diagnostic = {
+        "[d" = "goto_next";
+        "]d" = "goto_prev";
+      };
+      keymaps.lspBuf = {
+        K = "hover";
+        gD = "references";
+        gd = "definition";
+        gi = "implementation";
+        gt = "type_definition";
+      };
+      servers.pyright.enable = true;
+      servers.nil-ls.enable = true;
+      servers.jsonls.enable = true;
+      servers.lua-ls.enable = true;
+      servers.marksman.enable = true;
+    };
+
+    lint = {
+      enable = true;
+      linters.ruff.cmd = ruff;
+      lintersByFt.python = ["ruff"];
+    };
+
+    treesitter = {
+      enable = true;
+      settings = {
+        auto_install = true;
+        highlight.enable = true;
+      };
+    };
+
+    telescope = {
+      enable = true;
+      keymaps = {
+        "<leader>h" = "find_flies";
+        "<leader>g" = "live_grep";
+        "<leader>df" = "lsp_references";
+      };
+    };
+
+    harpoon = {
+      enable = true;
+      keymaps = {
+        addFile = "<leader>a";
+        toggleQuickMenu = "<leader>s";
+        navFile = {
+          "1" = "<C-A-j>";
+          "2" = "<C-A-k>";
+          "3" = "<C-A-l>";
+          "4" = "<C-A-;>";
+        };
+      };
+    };
+
+    cmp = {
+      enable = true;
+      settings = {
+        autoEnableSources = true;
+        snippet.expand = "luasnip";
+        experimental.ghost_text = true;
+        preselect = "cmp.PreselectMode.Item";
+        formatting.fields = ["kind" "abbr" "menu"];
+
+        sources = [
+          {name = "nvim_lsp";}
+          {name = "luasnip";}
+          {name = "nvim_lua";}
+          {name = "calc";}
+          {name = "path";}
+          {name = "buffer";}
+        ];
+        mapping = {
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<C-p>" = "cmp.mapping(function() if cmp.visible() then cmp.select_prev_item({behavior = 'select'}) else cmp.complete() end end)";
+          "<C-n>" = "cmp.mapping(function() if cmp.visible() then cmp.select_next_item({behavior = 'select'}) else cmp.complete() end end)";
+        };
+        window = {
+          completion = {
+            border = "rounded";
+            winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None";
+          };
+          documentation.border = "rounded";
+        };
+      };
+    };
+
+    luasnip = {
+      enable = true;
+      extraConfig = {
+        enable_autosnippets = true;
+        store_selection_keys = "<Tab>";
+      };
+      fromVscode = [
+        {
+          lazyLoad = true;
+          paths = "${pkgs.vimPlugins.friendly-snippets}";
+        }
+      ];
+    };
+
+    gitsigns.enable = true;
+
+    neogit.enable = true;
+
+    oil.enable = true;
+
+    nvim-autopairs.enable = true;
+
+    surround.enable = true;
+
+    comment.enable = true;
+
+    todo-comments.enable = true;
+
+    lualine.enable = true;
+
+    fidget.enable = true;
+
+    cmp-nvim-lsp.enable = true; # lsp
+
+    cmp-calc.enable = true;
+
+    cmp-buffer.enable = true;
+
+    cmp-path.enable = true; # file system paths
+
+    cmp_luasnip.enable = true; # snippets
+
+    cmp-cmdline.enable = true; # autocomplete for cmdlinep
+  };
+
+  extraPlugins = with pkgs.vimPlugins; [
+    vim-visual-multi
+  ];
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>g";
+      action = "<cmd>Neogit<cr>";
+    }
+  ];
+}
