dfd38da9f5a04d957f369ab0c596d8fd150c7382
Ryan Moffitt <rmoffitt@visiostack.com>
2025-01-16 21:14:06 -0500

    java

--- config/default.nix
+++ config/default.nix
@@ -84,6 +84,7 @@ in {
         gt = "type_definition";
       };
       servers = {
+	java_language_server.enable = true;
         pyright.enable = true;
         dartls.enable = true;
         nixd.enable = true;
