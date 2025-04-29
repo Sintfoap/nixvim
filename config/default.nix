8c7aa35a38bfb7e81b73d7f62103dfa53c9d7bdc
Sintfoap <rmoff938@students.bju.edu>
2025-04-28 22:10:27 -0400

    omnisharp2.3

--- config/default.nix
+++ config/default.nix
@@ -104,7 +104,7 @@ in {
         svelte.enable = true;
         clangd.enable = true;
         clojure_lsp.enable = true;
-	csharp_ls.enable = true;
+        csharp_ls.enable = true;
         tinymist = {
           enable = true;
           extraOptions.offset_encoding = "utf-8";
@@ -265,6 +265,37 @@ in {
 
   extraConfigLua = ''
     require("typst-preview").setup()
+    local _border = "rounded"
+
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+      vim.lsp.handlers.hover, {
+        border = _border
+      }
+    )
+
+    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+      vim.lsp.handlers.signature_help, {
+        border = _border
+      }
+    )
+
+    vim.diagnostic.config{
+      float={border=_border}
+    };
+
+    require('lspconfig.ui.windows').default_options = {
+      border = _border
+    }
+
+    config = function(_, opts)
+      local lspconfig = require('lspconfig')
+      for server, config in pairs(opts.servers) do
+        -- passing config.capabilities to blink.cmp merges with the capabilities in your
+        -- `opts[server].capabilities, if you've defined it
+        config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+        lspconfig[server].setup(config)
+      end
+    end;
   '';
 
   extraPlugins = with pkgs.vimPlugins; [
